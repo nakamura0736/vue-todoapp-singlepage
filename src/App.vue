30793de6f5049276a310629f536251abcf0d1546
nakamura0736 <tel07360102@gmail.com>
2020-09-23 18:53:27 +0900

    BootstrapでUI整え

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <h3>My TODO</h3>
-      <input v-model="newTodo" placeholder="Input here..">
-      <button v-on:click="addTodo()">ADD</button>
-      <h5>ToDo List</h5>
-      <ul>
-        <li v-for="(todo, i) in todos" v-bind:key="i">
+    <b-container class="mt-5">
+      <h3>My TODO</h3>
+      <b-form inline>
+        <label class="sr-only" for="inline-form-input-name">Todo</label>
+        <b-input
+          v-model = "newTodo"
+          id="inline-form-input-name"
+          class="mb-2 mr-sm-2 mb-sm-0"
+          placeholder="Input here.."
+        ></b-input>
+        <b-button variant="success"  v-on:click="addTodo()">ADD</b-button>
+      </b-form>
+
+      <h5 class="mt-5">ToDo List</h5>
+      <b-list-group>
+        <b-list-group-item v-for="(todo, i) in todos" v-bind:key="i" class="list-group-item d-flex justify-content-between align-items-center">
           {{ todo }}
-          <button v-on:click="deleteTodo(i)">DELETE</button>
-        </li>
-      </ul>
+          <b-button variant="success" v-on:click="deleteTodo(i)">DELETE</b-button>
+        </b-list-group-item>
+      </b-list-group>
+    </b-container>
   </div>
 </template>
 
